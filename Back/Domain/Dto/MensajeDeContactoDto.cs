7b5c14e838545f5576987bb1cdde873be26c453a
bsp98 <b.soutoperrone@gmail.com>
2025-08-14 19:28:16 -0300

    Mensaje de contacto

---
+++ Back/Domain/Dto/MensajeDeContactoDto.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Exceptions;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dto
+{
+    public class MensajeDeContactoDto : IValidable
+    {
+        public string Nombre {  get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Descripcion {  get; set; }
+
+        public MensajeDeContactoDto(string nombre, string apellido, string email,string telefono, string descripcion)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Telefono = telefono;
+            Descripcion = descripcion;
+        }
+
+        public void Validar() 
+        {
+
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new DatoIncorrectoException("El mensaje debe tener un nombre");
+            }
+
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                throw new DatoIncorrectoException("El mensaje debe tener un apellido");
+            }
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatoIncorrectoException("El mensaje debe tener un email");
+            }
+            if (string.IsNullOrEmpty(Telefono))
+            {
+                throw new DatoIncorrectoException("El mensaje debe tener un telefono");
+            }
+            if (string.IsNullOrEmpty(Descripcion))
+            {
+                throw new DatoIncorrectoException("El mensaje debe tener una descripcion");
+            }
+        }
+
+    }
+}
