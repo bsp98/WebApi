26de47ae0796ce0d62cf33a5ff2acd85abeb0e42
bsp98 <b.soutoperrone@gmail.com>
2025-08-31 13:55:09 -0300

    .

--- Front/src/services/urlBaseService.js
+++ Front/src/services/urlBaseService.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const urlBaseService = () => {
     const urlAzure = "https://webapictvwapa.azurewebsites.net/"
     const urlLocal = "http://localhost:5164/"
-    const urlBase = urlLocal;
+    const urlBase = urlAzure;
     return {
         urlBase,
     }
