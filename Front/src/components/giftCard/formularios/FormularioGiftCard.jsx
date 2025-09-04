e09d452000479211a673cab11828c0c6ec44136e
Rodrigo-Silva-Romero <rodrigosilvaromero14@gmail.com>
2025-09-03 21:51:02 -0300

    solucion de la gift card

--- Front/src/components/giftCard/formularios/FormularioGiftCard.jsx
+++ Front/src/components/giftCard/formularios/FormularioGiftCard.jsx
@@ -14,13 +14,13 @@ export const FormularioGiftCard = ({ onSubmit, formRef, cliente = null, error })
 
                 <div className='container_gruop_datos'>
 
-                    <InputForm tipo={"text"} id={"nombreComprador"} labelDescription={"Tu nombre :"} name={"nombreComprador"} placeholder={"Ingresa tu nombre"} esRequerido={true} isDisabled={cliente ? true : false} value={cliente ? cliente.nombre : ""} />
+                    <InputForm tipo={"text"} id={"nombreComprador"} labelDescription={"Tu nombre :"} name={"nombreComprador"} placeholder={"Ingresa tu nombre"} esRequerido={true} isDisabled={cliente?.nombre ? true : false} value={cliente ? cliente.nombre : ""} />
 
-                    <InputForm tipo={"text"} id={"emailComprador"} labelDescription={"Tu email:"} name={"emailComprador"} placeholder={"Ingresa tu email"} esRequerido={true} isDisabled={cliente ? true : false} value={cliente ? cliente.email : ""} />
+                    <InputForm tipo={"text"} id={"emailComprador"} labelDescription={"Tu email:"} name={"emailComprador"} placeholder={"Ingresa tu email"} esRequerido={true} isDisabled={cliente?.email ? true : false} value={cliente ? cliente.email : ""} />
                 </div>
 
                 <div className='container_gruop_datos'>
-                    <InputForm tipo={"text"} id={"celularComprador"} labelDescription={"Tu celular:"} name={"celularComprador"} placeholder={"Ingresa tu celular"} esRequerido={true} isDisabled={cliente ? true : false} value={cliente ? cliente.celular : ""} />
+                    <InputForm tipo={"text"} id={"celularComprador"} labelDescription={"Tu celular:"} name={"celularComprador"} placeholder={"Ingresa tu celular"} esRequerido={true} isDisabled={cliente?.celular ? true : false} value={cliente ? cliente.celular : ""} />
 
                     <InputForm tipo={"text"} id={"nombreDestinatario"} labelDescription={"Nombre del destinatario:"} name={"nombreDestinatario"} placeholder={"Nombre de quien recibirá la gift card"} esRequerido={true} />
                 </div>
