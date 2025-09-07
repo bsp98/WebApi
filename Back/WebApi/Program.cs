9d0715003e6cbaeb8450f678d030b0f5a7ac7bb0
bsp98 <b.soutoperrone@gmail.com>
2025-09-06 23:56:38 -0300

    .

--- Back/WebApi/Program.cs
+++ Back/WebApi/Program.cs
@@ -71,7 +71,7 @@ namespace WebApi
             {
                 options.AddPolicy("PermitirFrontendLocalhost", policy =>
                 {
-                    policy.WithOrigins("http://localhost:5173", "https://delightful-bush-03f20bd10.1.azurestaticapps.net")
+                    policy.WithOrigins("http://localhost:5173", "https://icy-glacier-0d421d610.1.azurestaticapps.net")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
--- Front/src/services/urlBaseService.js
+++ Front/src/services/urlBaseService.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const urlBaseService = () => {
-    const urlAzure = "https://webapictvwapa.azurewebsites.net/"
+    const urlAzure = "https://icy-glacier-0d421d610.1.azurestaticapps.net/"
     const urlLocal = "http://localhost:5164/"
     const urlBase = urlAzure;
     return {
