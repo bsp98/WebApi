9378432b8ec583b2f3a4a5fd113adb129e5ecba3
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-10-04 16:07:53 -0300

    Cambio de servicios inactivos

--- Front/src/components/iu/cards/ContainerCards.jsx
+++ Front/src/components/iu/cards/ContainerCards.jsx
@@ -3,14 +3,24 @@ import './cards.css'
 import { Card } from './Card'
 
 export const ContainerCards = ({ data, action_btn }) => {
-
+    console.log(data)
     return (
         <div className='container_cards'>
-            
-            {data.map(dato => {
-               return <Card  key={dato.id} dato={dato} action_btn={action_btn}/>;
-            })}
-
+            {data.map(dato =>
+                dato.disponibilidad === 1 && (
+                    <Card key={dato.id} dato={dato} action_btn={action_btn} />
+                )
+            )}
         </div>
     )
+
+    /*  return (
+            <div className='container_cards'>
+                
+               {data.map(dato => {
+                   return <Card  key={dato.id} dato={dato} action_btn={action_btn}/>;
+                })}
+    
+            </div>
+        )*/
 }
